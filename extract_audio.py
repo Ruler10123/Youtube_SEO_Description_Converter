eaa8ba4c6f5a4df9ee5a737e29a229fcfd5d99bd
Ruler10123 <ruler10123@tamu.edu>
2025-11-09 22:50:45 -0600

    finished transcriptions with timestamps

--- extract_audio.py
+++ extract_audio.py
@@ -2,10 +2,9 @@ import subprocess
 import os
 
 folder_path = "C:/Projects/Coding/We_Can_Do_It/Youtube_SEO_Description_Converter/videos"
-output_folder = "C:/Projects/Coding/We_Can_Do_It/Youtube_SEO_Description_Converter/outputs"
+output_folder = "C:/Projects/Coding/We_Can_Do_It/Youtube_SEO_Description_Converter/audio"
 
 for filename in os.listdir(folder_path):
-    print(filename)
     file_path = os.path.join(folder_path, filename)
     if os.path.isfile(file_path):
         output_file_path = os.path.join(output_folder, f'{filename}.mp3')
--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,19 @@
 import whisper
 import os
 
-model = whisper.load_model("turbo")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+folder_path = "C:/Projects/Coding/We_Can_Do_It/Youtube_SEO_Description_Converter/audio"
+output_folder = "C:/Projects/Coding/We_Can_Do_It/Youtube_SEO_Description_Converter/transcriptions"
+
+model = whisper.load_model("medium.en")
+
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    if os.path.isfile(file_path):
+        output_file_path = os.path.join(output_folder, f'{filename}.txt')
+        result = model.transcribe(file_path, word_timestamps=True)
+        with open(output_file_path, "w") as file:
+            for segment in result["segments"]:
+                file.write(f"[{segment['start']:.2f}-{segment['end']:.2f}] {segment['text']}\n")
+            for word in segment["words"]:
+                file.write(f"  {word['word']} [{word['start']:.2f}-{word['end']:.2f}]\n")
+        print(f"Conversion complete! {output_file_path} has been created.")
